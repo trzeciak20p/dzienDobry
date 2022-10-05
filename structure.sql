5b7b5e18c876e6957f0398172de208861161b645
Marekse <m.trzeciak.p20@gmail.com>
2022-10-05 18:09:43 +0200

    wstępna struktura

--- structure.sql
+++ structure.sql
@@ -1,7 +1,7 @@
 
 
 create table nauczyciele(
-    id int primary key not null,
+    nauczyciel_id int primary key not null auto_increment,
     nazwisko varchar(50)  not null,
     imie varchar(30)  not null,
     dobry int, 
@@ -10,15 +10,22 @@ create table nauczyciele(
 
 create table dni(
     dzien date primary key not null,
-    nauczyciel int ,
+    nauczyciel int,
     dobry int, 
     nie_dobry int,
-    constraint c_nauczuciel foreign key (nauczyciel) references nauczyciele(id)
+    constraint c_nauczuciel foreign key (nauczyciel) references nauczyciele(nauczyciel_id)
 );
 
+create table glosy(
+    dzien date primary key not null,
+    uzytkownik_id int primary key not null auto_increment,
+    constraint c_glos_dzien foreign key (dzien) references dni(dzien),
+    constraint c_glos_uzytkownik foreign key (urzytkownik_id) references uzytkownicy(urzytkownik_id),
+);
 
-create table licznik(
-    dobry int,
-    nie_dobry int
+create table uzytkownicy(
+    uzytkownik_id int primary key not null auto_increment,
+    nazwa varchar(40) not null,
+    haslo password not null,  
 );
 
