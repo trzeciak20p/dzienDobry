d1d6a8bfdbe8d28f62f9fa514caeb4ea463ed770
Marekse <m.trzeciak.p20@gmail.com>
2022-10-08 14:51:26 +0200

    działa 3.0

--- structure.sql
+++ structure.sql
@@ -1,29 +1,26 @@
 
 create table nauczyciele(
-    nauczyciel_id smallint primary key not null auto_increment,
+    nauczyciel_id tinyint primary key not null auto_increment,
     nazwisko varchar(50)  not null,
     imie varchar(30)  not null,
     dobry int, 
     nie_dobry int
 ); 
 
-create table dni(
-    dzien_id smallint primary key not null auto_increment
-    dzien date not null,
-);
-
-create table glosy(
-    dzien_id smallint not null,
-    nauczyciel_id int not null,
-    uzytkownik_id int not null,
-    glos boolean,
-    constraint c_glos_dzien foreign key (dzien_id) references dni(dzien_id),
-    constraint c_glos_uzytkownik foreign key (urzytkownik_id) references uzytkownicy(urzytkownik_id),
-    constraint c_glos_nauczyciel foreign key (nauczyciel_id) references nauczyciele(nauczyciel_id)
-);
-
 create table uzytkownicy(
     uzytkownik_id smallint primary key not null auto_increment,
     nazwa varchar(40) not null,
-    haslo password not null,  
+    haslo varchar(40) not null
 );
+
+create table glosy(
+    glos_id int not null,
+    data date not null,
+    nauczyciel_id tinyint not null,
+    uzytkownik_id smallint not null,
+    glos boolean,
+    constraint fk_glos_uzytkownik foreign key (uzytkownik_id) references uzytkownicy(uzytkownik_id),
+    constraint fk_glos_nauczyciel foreign key (nauczyciel_id) references nauczyciele(nauczyciel_id)
+);
+
+
