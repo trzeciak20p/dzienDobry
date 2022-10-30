494a3684574001366e1c65ac1aabb5787a92862f
N1ktt <76476591+N1ktt@users.noreply.github.com>
2022-10-30 16:34:34 +0100

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO nauczyciele(imie,nazwisko,dobry,nie_dobry) VALUES("Dariusz","Bodzioch",0,0);
+INSERT INTO nauczyciele(imie,nazwisko,dobry,nie_dobry) VALUES("Daniel","Kuśka",0,0);
+INSERT INTO nauczyciele(imie,nazwisko,dobry,nie_dobry) VALUES("Katarzyna","Pietrzyk-Piotrowska",0,0);
+
+
+INSERT INTO uzytkownicy(nazwa,haslo) VALUEs("admin",SHA2('admmin',256));
